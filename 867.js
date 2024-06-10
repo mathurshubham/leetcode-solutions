7d6859350e5563a0ad053f63499cea9fc703bb12
shubham-mathur <mathurshubham@gmail.com>
2024-06-10 16:12:33 +0530

    ques 867; java solution -> correcting rows & columns for output matrix

--- 867.js
+++ 867.js
@@ -24,11 +24,11 @@ class Solution {
     public int[][] transpose(int[][] matrix){
         int r = matrix.length;
         int c = matrix[0].length;
-        int output[][] = new int[r][c] //initializing matrix in java
+        int output[][] = new int[c][r]; //initializing matrix in java
     
     
         for(int i = 0; i < r; i++){
-            for(int j = 0; i < c; j++){
+            for(int j = 0; j < c; j++){
                 output[j][i] = matrix[i][j];
             }
         }
